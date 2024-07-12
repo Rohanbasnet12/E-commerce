d6542c805808cc31e454f461796f0a12d94ca900
Rohanbasnet12 <rohanbmyname@gmail.com>
2024-07-12 08:30:37 -0700

    Completed Navbar Section

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -1,9 +1,37 @@
 import React from "react";
+import "./Home.css";
 
 const Home = () => {
   return (
-    <div id="home">
-      <h1>Home page</h1>
+    <div
+      id="home"
+      className="flex items-center w-full"
+      style={{ padding: "35px 28px" }}
+    >
+      <div className="homeLeft-section w-1/2">
+        <h1 className="text-[14vw]">Sage</h1>
+        <p className="text-3xl">Shop Your Vision, Live Your Creation</p>
+        <button
+          id="purchase-btn"
+          className="mt-24 text-l font-bold rounded-full py-2 px-4"
+        >
+          Purchase Now!
+        </button>
+      </div>
+      <div className="homeRight-section w-1/2 grid grid-cols-2 gap-8">
+        <div className="box">
+          <img src="../images/kids.jfif" alt="" />
+        </div>
+        <div className="box">
+          <img src="../images/kids.jfif" alt="" />
+        </div>
+        <div className="box">
+          <img src="../images/kids.jfif" alt="" />
+        </div>
+        <div className="box">
+          <img src="../images/kids.jfif" alt="" />
+        </div>
+      </div>
     </div>
   );
 };
--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -1,14 +1,34 @@
 import React from "react";
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
-    <div id="navbar">
-      <nav>
-        <div className="nav-head"></div>
-        <div className="menu"></div>
-        <div className="nav-icons"></div>
-      </nav>
-    </div>
+    <nav id="navbar" className="flex items-center justify-between">
+      <div className="nav-head">
+        <h1 className="text-3xl font-bold">Sage</h1>
+      </div>
+      <div className="menu text-white font-medium">
+        <ul className="flex items-center justify-center gap-12 cursor-pointer">
+          <li>Home</li>
+          <li>Men's</li>
+          <li>Women's</li>
+          <li>Kid's</li>
+        </ul>
+      </div>
+      <div className="nav-icons flex items-center gap-6">
+        <i className="fa-solid fa-heart"></i>
+        <i className="fa-solid fa-cart-shopping"></i>
+        <div className="login">
+          <button className="text-white">
+            {" "}
+            <span>
+              <i className="fa-solid fa-user text-black pr-2"></i>
+            </span>{" "}
+            Login
+          </button>
+        </div>
+      </div>
+    </nav>
   );
 };
 
