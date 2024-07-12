786f149dd721ccbe9d3254a679245424261f1a72
Rohanbasnet12 <rohanbmyname@gmail.com>
2024-07-12 08:45:23 -0700

    Completed the home section

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -20,16 +20,60 @@ const Home = () => {
       </div>
       <div className="homeRight-section w-1/2 grid grid-cols-2 gap-8">
         <div className="box">
-          <img src="../images/kids.jfif" alt="" />
+          <div
+            className="box-wrapper w-full h-full flex items-center justify-center flex-col gap-4 p-4"
+            style={{ background: "rgba(0, 0, 0, .4)" }}
+          >
+            <p className="description text-center">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              Nesciunt, pariatur.
+            </p>
+            <button className="border p-3 text-black bg-slate-200 font-medium">
+              Discover More
+            </button>
+          </div>
         </div>
         <div className="box">
-          <img src="../images/kids.jfif" alt="" />
+          <div
+            className="box-wrapper w-full h-full flex items-center justify-center flex-col gap-4 p-4"
+            style={{ background: "rgba(0, 0, 0, .4)" }}
+          >
+            <p className="description text-center">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              Nesciunt, pariatur.
+            </p>
+            <button className="border p-3 text-black bg-slate-200 font-medium">
+              Discover More
+            </button>
+          </div>
         </div>
         <div className="box">
-          <img src="../images/kids.jfif" alt="" />
+          <div
+            className="box-wrapper w-full h-full flex items-center justify-center flex-col gap-4 p-4"
+            style={{ background: "rgba(0, 0, 0, .4)" }}
+          >
+            <p className="description text-center">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              Nesciunt, pariatur.
+            </p>
+            <button className="border p-3 text-black bg-slate-200 font-medium">
+              Discover More
+            </button>
+          </div>
         </div>
         <div className="box">
-          <img src="../images/kids.jfif" alt="" />
+          <div
+            className="box-wrapper w-full h-full flex items-center justify-center flex-col gap-4 p-4"
+            style={{ background: "rgba(0, 0, 0, .4)" }}
+          >
+            <p className="description text-center">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              Nesciunt, pariatur.
+            </p>
+            <button className="border p-3 text-black bg-slate-200 font-medium">
+              Discover More
+            </button>
+          </div>
         </div>
       </div>
     </div>
