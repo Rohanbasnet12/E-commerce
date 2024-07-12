31b1081e662dd5d48da376e05409db05672ce2de
Rohanbasnet12 <rohanbmyname@gmail.com>
2024-07-12 07:47:10 -0700

    Integrating Tailwind CSS

---
+++ src/component/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div id="navbar">
+      <nav>
+        <div className="nav-head"></div>
+        <div className="menu"></div>
+        <div className="nav-icons"></div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
